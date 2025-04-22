ef15af0110ac38086ef7243ceedbb398d3b54fd5
Thauã Gabriel Sousa e Silva <tagashi.dev@gmail.com>
2025-04-22 17:05:08 -0300

    feat: realizar exercicio 3 - cap 2

---
+++ dist/capitulo2/exe03/exe03.js
@@ -0,0 +1,24 @@
+"use strict";
+// Usando `switch`, crie um programa que dado o número de uma linguagem imprima: - 1 → "TypeScript" - 2 → "JavaScript" - 3 → "Python" - qualquer outro valor → "Linguagem não cadastrada"
+function escolherLinguagem(numeroLinguagem) {
+    let linguagem = "";
+    switch (numeroLinguagem) {
+        case 1:
+            linguagem = "TypeScript";
+            break;
+        case 2:
+            linguagem = "JavaScript";
+            break;
+        case 3:
+            linguagem = "Python";
+            break;
+        default:
+            linguagem = "Linguagem não cadastrada";
+            break;
+    }
+    return linguagem;
+}
+let linguagem = escolherLinguagem(1);
+console.log(linguagem);
+linguagem = escolherLinguagem(10);
+console.log(linguagem);
