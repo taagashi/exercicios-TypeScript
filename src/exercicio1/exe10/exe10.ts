46d1e1c5c15a57a8fd6b7b1afcae90e45b9d9855
Thauã Gabriel Sousa e Silva <tagashi.dev@gmail.com>
2025-04-22 08:50:07 -0300

    feat: realizar exercicio 10

---
+++ src/exercicio1/exe10/exe10.ts
@@ -0,0 +1,9 @@
+// Crie uma função chamada calcularAreaRetangulo que receba largura e altura e retorne a área (largura * altura).
+
+function calcularAreaRetangulo(largura: number, altura: number) {
+    return (largura * altura);
+}
+
+const areaCalculada = calcularAreaRetangulo(12, 5);
+
+console.log(areaCalculada);
