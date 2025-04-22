b4b1c7398c3639002d8b648e20b2b2ac46e408d2
Thauã Gabriel Sousa e Silva <tagashi.dev@gmail.com>
2025-04-22 08:13:14 -0300

    feat: realizar exercicio 1

---
+++ dist/exercicio1/exe01/exe01.js
@@ -0,0 +1,4 @@
+"use strict";
+// Crie uma variável chamada nome do tipo string e atribua o seu nome.
+const nome = "Thauã Gabriel Sousa e Silva";
+console.log(nome);
---
+++ src/exercicio1/exe01/exe01.ts
@@ -0,0 +1,4 @@
+// Crie uma variável chamada nome do tipo string e atribua o seu nome.
+
+const nome: string = "Thauã Gabriel Sousa e Silva";
+console.log(nome);
