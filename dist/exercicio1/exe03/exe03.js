910d0d1eafc1746f72ecbdfba7d93c48e9fc414b
Thauã Gabriel Sousa e Silva <tagashi.dev@gmail.com>
2025-04-22 08:17:13 -0300

    feat: realizar exercicio 3

---
+++ dist/exercicio1/exe03/exe03.js
@@ -0,0 +1,6 @@
+"use strict";
+// Crie uma função chamada saudacao que receba um nome (string) como parâmetro e retorne: "Olá, <nome>!".
+function saudacao(nome) {
+    return `Olá, ${nome}!`;
+}
+console.log(saudacao("Thauã"));
