29095d9d09fb8e0beae1f09025afd4b7492a4afa
Thauã Gabriel Sousa e Silva <tagashi.dev@gmail.com>
2025-04-22 16:56:44 -0300

    feat: realizar exercicio 2 - cap 2

---
+++ dist/capitulo2/exe02/exe02.js
@@ -0,0 +1,6 @@
+"use strict";
+// Peça para o aluno criar um programa que verifique se um número é par ou ímpar, utilizando o operador ternário.
+function parImpar(numero) {
+    console.log((numero % 2 == 0) ? "par" : "impar");
+}
+parImpar(1);
---
+++ src/capitulo2/exe02/exe02.ts
@@ -0,0 +1,7 @@
+// Peça para o aluno criar um programa que verifique se um número é par ou ímpar, utilizando o operador ternário.
+
+function parImpar(numero: number): void {
+    console.log((numero % 2 == 0) ? "par"  : "impar");
+}
+
+parImpar(1);
